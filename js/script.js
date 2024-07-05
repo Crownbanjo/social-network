2f2434c70cb563dd12124408ab845659eec387d1
Adebanjo Micheal <adebanjo_micheal@yahoo.com>
2024-07-05 14:32:13 +0100

    added function

---
+++ js/script.js
@@ -0,0 +1,17 @@
+document.getElementById("git-btn").addEventListener("click", function () {
+  window.location.href = "https://github.com/Crownbanjo";
+});
+
+document.getElementById("ig-btn").addEventListener("click", function () {
+  window.location.href = "https://www.instagram.com/crownbanjo.ng/";
+});
+document.getElementById("linkedin-btn").addEventListener("click", function () {
+  window.location.href =
+    "https://www.linkedin.com/in/adebanjo-micheal-970710167/";
+});
+document.getElementById("twit-btn").addEventListener("click", function () {
+  window.location.href = "https://x.com/Tallmikky";
+});
+document.getElementById("insta-btn").addEventListener("click", function () {
+  window.location.href = "https://www.instagram.com/micheal_crownbanjo/";
+});
